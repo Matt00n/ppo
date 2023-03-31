e9bca45829eafce13c0c62e460de7b0261dc9e70
Matt00n <matthiaslehmann@MacBook-Pro.fritz.box>
2023-03-31 13:56:36 +0200

    import VecNormalize

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .learner import PPO
 from .utils.utils import obs_as_tensor, configure_logger, safe_mean
-from .envs import make_vec_env
+from .envs import make_vec_env, VecNormalize
 
 __all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "PPO"]
